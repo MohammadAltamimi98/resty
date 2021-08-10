5abc903ba144eac99b4de8ce78f0173fb41e7f16
MohammadAltamimi98 <m98altamimi@gmail.com>
2021-08-10 20:31:11 +0300

    commit

--- src/components/results/Results.jsx
+++ src/components/results/Results.jsx
@@ -1,12 +1,11 @@
 import './Results.scss';
 
-function Results(data) {
+function Results(props) {
   return (
     <section>
-      <pre>{data ? JSON.stringify(data, undefined, 2) : null}</pre>
+      <pre>{props.data ? JSON.stringify(props.data, undefined, 2) : null}</pre>
     </section>
   );
 }
-
 export default Results;
 
