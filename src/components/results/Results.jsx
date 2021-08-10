4521d14576da84abe477fe831b90976b562f4c6a
MohammadAltamimi98 <m98altamimi@gmail.com>
2021-08-10 21:05:10 +0300

    commit

--- src/components/results/Results.jsx
+++ src/components/results/Results.jsx
@@ -1,9 +1,11 @@
 import './Results.scss';
+import '../loading/Loading';
+import Loading from '../loading/Loading';
 
 function Results(props) {
   return (
     <section>
-      <pre>{props.data ? JSON.stringify(props.data, undefined, 2) : null}</pre>
+      <pre>{props.data ? JSON.stringify(props.data, undefined, 2) : <Loading />}</pre>
     </section>
   );
 }
